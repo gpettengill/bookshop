d1cf5d6425be3ed4b87bc34f71fc474cb1db7d7f
gpettengill <gpettengill@gmail.com>
2021-11-24 12:37:57 -0500

    Clean up.. added files from later exercises in the class

--- rest-server/src/main/java/com/readlearncode/dukesbookshop/RESTConfig.java
+++
@@ -1,8 +0,0 @@
-package com.readlearncode.dukesbookshop;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RESTConfig extends Application {
-}
--- rest-server/src/main/java/com/readlearncode/dukesbookshop/rest/AuthorResource.java
+++
@@ -1,9 +0,0 @@
-package com.readlearncode.dukesbookshop.rest;
-
-import javax.ejb.Stateless;
-import javax.ws.rs.Path;
-
-@Stateless
-@Path("/authors")
-public class AuthorResource {
-}
